81ed8738cf75e82b467c8979db2888dcceb1a97a
Aditya Singh <75111366+AdityaSingh12@users.noreply.github.com>
2024-09-09 22:15:05 +0530

    Add files via upload

---
+++ 9Sept_GFG.py
@@ -0,0 +1,11 @@
+"""
+Sort 0s, 1s and 2s
+
+Given an array arr containing only 0s, 1s, and 2s. Sort the array in ascending order.
+"""
+
+class Solution:
+    # Function to sort an array of 0s, 1s, and 2s
+    def sort012(self, arr):
+        arr.sort()
+        return arr
