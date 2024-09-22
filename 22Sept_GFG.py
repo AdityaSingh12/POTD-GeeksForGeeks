72d2bdd7284f3d925b4ec5466a843ff396b07b63
Aditya Singh <75111366+AdityaSingh12@users.noreply.github.com>
2024-09-22 22:40:40 +0530

    Add files via upload

---
+++ 22Sept_GFG.py
@@ -0,0 +1,40 @@
+"""
+Longest Prefix Suffix
+
+Given a string of characters, find the length of the longest proper prefix which is also a proper suffix.
+
+Note: Prefix and suffix can be overlapping but they should not be equal to the entire string.
+"""
+
+class Solution:
+    def lps(self, s: str) -> int:
+        n = len(s)
+        if n == 0:
+            return 0
+
+        lpsArr = [0] * n
+        j = 0
+
+        for i in range(1, n):
+            while j > 0 and s[i] != s[j]:
+                j = lpsArr[j - 1]
+            if s[i] == s[j]:
+                j += 1
+            lpsArr[i] = j
+
+        return lpsArr[n - 1]		
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    T = int(input())
+    for i in range(T):
+        s = input()
+
+        ob = Solution()
+        answer = ob.lps(s)
+        print(answer)
+
+# } Driver Code Ends
