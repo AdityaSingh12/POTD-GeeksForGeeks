efdd80c0c201c13243bc559d6bd401e8e00fd426
Aditya Singh <75111366+AdityaSingh12@users.noreply.github.com>
2024-10-01 22:35:07 +0530

    Add files via upload

---
+++ 1Oct_GFG.py
@@ -0,0 +1,90 @@
+"""
+Multiply two Linked Lists
+Given elements as nodes of the two singly linked lists. 
+The task is to multiply these two linked lists, say L1 and L2.
+
+Note: The output could be large take modulo 10^9+7.
+"""
+
+
+class Solution:
+    def multiply_two_lists(self, first, second):
+        num1 = 0
+        num2 = 0
+        MOD = 1000000007
+        while first is not None:
+            num1 = (num1 * 10 + first.data) % MOD
+            first = first.next
+        while second is not None:
+            num2 = (num2 * 10 + second.data) % MOD
+            second = second.next
+        return (num1 * num2) % MOD
+
+
+
+
+
+#{ 
+ # Driver Code Starts
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def new_node(data):
+    return Node(data)
+
+
+def push(head_ref, new_data):
+    new_Node = new_node(new_data)
+    new_Node.next = head_ref[0]
+    head_ref[0] = new_Node
+
+
+def reverse(r):
+    prev = None
+    cur = r[0]
+    while cur is not None:
+        nxt = cur.next
+        cur.next = prev
+        prev = cur
+        cur = nxt
+    r[0] = prev
+
+
+def free_list(Node):
+    while Node:
+        temp = Node
+        Node = Node.next
+        del temp
+
+
+def print_list(Node):
+    while Node:
+        print(Node.data, end=" ")
+        Node = Node.next
+    print()
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        first, second = [None], [None]
+        arr = list(map(int, input().split()))
+        for num in arr:
+            push(first, num)
+
+        brr = list(map(int, input().split()))
+        for num in brr:
+            push(second, num)
+
+        reverse(first)
+        reverse(second)
+
+        ob = Solution()
+        res = ob.multiply_two_lists(first[0], second[0])
+        print(res)
+
+# } Driver Code Ends
