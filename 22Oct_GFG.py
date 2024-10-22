b0528028ef07116736664a2e6c09bb7810e3da7e
Aditya Singh <75111366+AdityaSingh12@users.noreply.github.com>
2024-10-22 19:15:26 +0530

    Add files via upload

---
+++ 22Oct_GFG.py
@@ -0,0 +1,46 @@
+"""
+Sub-arrays with equal number of occurences
+
+Given an array arr[] and two integers say, x and y, 
+find the number of sub-arrays in which the number of 
+occurrences of x is equal to the number of occurrences of y.
+"""
+
+#User function Template for python3
+
+class Solution:
+    def sameOccurrence(self, arr, x, y):
+        diffCount = {}  
+        diff = 0        
+        result = 0
+
+        diffCount[0] = 1
+
+        for i in arr:
+            if i == x:
+                diff += 1  
+            elif i == y:
+                diff -= 1  
+
+            result += diffCount.get(diff, 0)
+
+            diffCount[diff] = diffCount.get(diff, 0) + 1
+
+        return result
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        arr = list(map(int, input().strip().split()))
+        x = int(input().strip())
+        y = int(input().strip())
+        ob = Solution()
+        ans = ob.sameOccurrence(arr, x, y)
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends
